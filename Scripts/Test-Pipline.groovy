451d6e6270d2a25f67db66da04d55328cd7a627b
miyamoto-tatsuki <i.love.wolf090@gmail.com>
2022-03-13 10:07:07 +0900

    first commit

---
+++ Scripts/Test-Pipline.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    triggers
+    {
+        cron("H 0-6 * * *")
+        
+    }
+    stages {
+        stage('Example Build') {
+            steps {
+                echo "Test"
+            }
+        }
+    }
+}
